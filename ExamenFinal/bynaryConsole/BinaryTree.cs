d495ded9df36879252604bd8d35aded628cd87df
saraft08 <110129816+saraft08@users.noreply.github.com>
2023-11-17 16:05:12 -0500

    Add files via upload

---
+++ ExamenFinal/bynaryConsole/BinaryTree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Binary_Tree
+{
+    using System;
+
+    class BinaryTree
+    {
+        public int Value { get; set; }
+        public BinaryTree Left { get; set; }
+        public BinaryTree Right { get; set; }
+
+        public BinaryTree(int value)
+        {
+            Value = value;
+            Left = Right = null;
+        }
+    }
+}
