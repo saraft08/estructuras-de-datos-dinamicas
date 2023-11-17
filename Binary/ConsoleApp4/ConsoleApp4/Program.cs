d9b0de56e5e0f1e05da2f8cdb8e5f1e65ad7d9dd
saraft08 <110129816+saraft08@users.noreply.github.com>
2023-11-17 15:38:56 -0500

    Add files via upload

---
+++ Binary/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Binary_Tree
+{
+
+
+    class Program
+    {
+        static void Main()
+        {
+            // Construir el árbol de ejemplo
+            BinaryTree tree = new BinaryTree(1)
+            {
+                Left = new BinaryTree(2)
+                {
+                    Left = new BinaryTree(4)
+                    {
+                        Right = new BinaryTree(9)
+                    }
+                },
+                Right = new BinaryTree(3)
+                {
+                    Left = new BinaryTree(6),
+                    Right = new BinaryTree(7)
+                }
+            };
+
+            // Realizar el recorrido en orden con la función de devolución de llamada
+            TreeTraversal.InOrderTraversal(tree, Callback.Execute);
+        }
+    }
+}
---
+++ Binary/ConsoleApp4/ConsoleApp4/TreeTraversal.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace Binary_Tree
+{
+    class TreeTraversal
+    {
+        public static void InOrderTraversal(BinaryTree root, Action<BinaryTree> callback)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            BinaryTree current = root;
+            BinaryTree predecessor;
+
+            while (current != null)
+            {
+                if (current.Left == null)
+                {
+                    callback(current);
+                    current = current.Right;
+                }
+                else
+                {
+                    predecessor = current.Left;
+
+                    while (predecessor.Right != null && predecessor.Right != current)
+                    {
+                        predecessor = predecessor.Right;
+                    }
+
+                    if (predecessor.Right == null)
+                    {
+                        predecessor.Right = current;
+                        current = current.Left;
+                    }
+                    else
+                    {
+                        predecessor.Right = null;
+                        callback(current);
+                        current = current.Right;
+                    }
+                }
+            }
+        }
+    }
+}
