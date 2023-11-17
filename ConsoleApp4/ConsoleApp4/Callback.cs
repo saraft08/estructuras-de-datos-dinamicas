a0fc087be9400c4fb1603897dd57de962fb0b271
saraft08 <110129816+saraft08@users.noreply.github.com>
2023-11-17 15:51:25 -0500

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Callback.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace Binary_Tree
+{
+    class Callback
+    {
+        public static void Execute(BinaryTree node)
+        {
+            // Aquí se llama a la función de devolución de llamada
+            Console.WriteLine($"callback({node.Value})");
+        }
+    }
+}
